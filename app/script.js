db846ac9a6ff6de2b7971b15c5f6b2337ab4faf8
Caio <108128119+DocCaio@users.noreply.github.com>
2023-12-18 16:38:59 -0300

    up

--- app/script.js
+++ app/script.js
@@ -1,8 +1,25 @@
+let servico = document.getElementsByClassName('servicos_main-div');
+servico.innerHTML = `<div class="servicos_main-div-grid">
+<img class="servicos_main-div-img" src="/images/icon-access-anywhere.svg" alt="computador">
+<h3 class="servicos_main-div-text">Acesse seus arquivos em qualquer lugar.</h3>      
+<p class="servicos_main-div-paragrafo">Você consigue acessar sesus arquivos pelo celula ,teblet ou desktop.</p>
+</div>
 
+<div  class="servicos_main-div-grid">
+<img  class="servicos_main-div-img"src="/images/icon-security.svg" alt="Shild">
+<h3 class="servicos_main-div-text">A segurança que você pode confiar.</h3>     
+<p class="servicos_main-div-paragrafo">Segurança 24 horas por dia com os profissionais mais capacitados do mercado.</p>
+</div>
 
-const servicos= document.createElement('.servicos_main-div')
-servicos.innerHTML = `
+<div  class="servicos_main-div-grid">
+<img class="servicos_main-div-img" src="/images/icon-collaboration.svg" alt="Check">
+<h3 class="servicos_main-div-text">Segurança em compartilhamento de dados.</h3>     
+<p class="servicos_main-div-paragrafo">Nossa segurança possui criptografia ponta a ponta , compartilhamento seguro e sem risco.</p>
+</div>
 
-
-   
-`
+<div  class="servicos_main-div-grid">
+<img class="servicos_main-div-img" src="/images/icon-any-file.svg" alt="folder">
+<h3 class="servicos_main-div-text">Armazene qualquer coisa.</h3>      
+<p class="servicos_main-div-paragrafo">Salve sesus dados sem medo de perde-los , nosso sitema de nuvem possui asmais novas tecnologias.</p>
+</li>
+</div>`
